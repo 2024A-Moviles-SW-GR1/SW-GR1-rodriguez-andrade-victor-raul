3d5f1c62e0c27c5560181787369e2fb783ef3a2f
Vikus7 <victor.rodriguez01@epn.edu.ec>
2024-08-18 00:07:47 -0500

    Homework 3 recycler views

---
+++ Deber03/app/src/main/java/com/example/deber03/ContenidoNotificaciones.kt
@@ -0,0 +1,86 @@
+package com.example.deber03
+
+class ContenidoNotificaciones {
+    companion object{
+        val notificacionesList = listOf(
+            Notificaciones(
+                "https://maicito.com/images/2019/09/02/emprendedor%20o%20empresario%20¿con%20cual%20te%20identificas_.png",
+                "Pedro Pedro",
+                "ha visto tu perfil.",
+                "Revisa todas las vistas a tu perfil.",
+                "7h"
+            ),
+            Notificaciones(
+                "https://fis.epn.edu.ec/images/logo-FIS-sin-fondo.png",
+                "FIS-EPN",
+                "ha",
+                " compartido una actualización: AHORA| El Dr. Trquino Sánchez Al...",
+                "9h"
+            ),
+            Notificaciones(
+                "https://www.jornalcontabil.com.br/images/noticias/74578/caa9f8c34a16b744c3517318227c4af1.jpg",
+                "Jorge Nitales",
+                "ha publicado",
+                "contenido: Recibes una llamada para una posición, ya sea que estés buscando o no, te llena de...",
+                "10h"
+            ),
+            Notificaciones(
+                "https://i.pinimg.com/550x/57/ef/a8/57efa8133a9f171d07d4eec266b7d530.jpg",
+                "Susasna Horia",
+                "ha compartido",
+                " una imagen",
+                "11h"
+            ),
+            Notificaciones(
+                "https://www.ceupe.mx/images/pexelsandreapiacquadio716411.jpg",
+                "Linus Telling",
+                "ha publicado",
+                "un artículo: El sector bancario está enfrentando retos importantes que están cambiando la manera en que l...",
+                "12h"
+            ),
+            Notificaciones(
+                "https://www.israeleconomico.com/wp-content/uploads/2020/11/MASTERCARD-ISRAEL-2.jpg",
+                "Ana Condita",
+                "ha compartido",
+                "contenido: ¡Únete a Procamaronex y conviértete en parte de nuestra familia, postúlate ahora!",
+                "13h"
+            ),
+            Notificaciones(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVVFnWA7rBvlXtzrlcgVgkyf-O_dRQdD_Qdw&s",
+                "Plasticidad",
+                "ha publicado",
+                "algo: ¡Todos somos Arca Continental! Nuestros socios tenderos y recicladores...",
+                "14h"
+            ),
+            Notificaciones(
+                "https://susociodenegocios.com/wp-content/uploads/2015/05/empresario-exitoso-latino.jpg",
+                "Rodolfo Curino",
+                "ha compartido",
+                "contenido: Recibes una llamada para una posición, ya sea que estés buscando o no, te llena de....",
+                "5h"
+            ),
+            Notificaciones(
+                "https://adiumpharma.com/wp-content/uploads/2021/12/isologo-2.png",
+                "Grupo Linces",
+                "ha publicado",
+                "algo: ¡En Medicamenta tenemos una oportunidad para ti! Sé parte de nuestro programa...",
+                "6h"
+            ),
+            Notificaciones(
+                "https://static.vecteezy.com/system/resources/previews/014/228/037/non_2x/businesswoman-at-work-photo.jpg",
+                "Marcia Xime",
+                "ha publicado",
+                "contenido",
+                "6h"
+            ),
+            Notificaciones(
+                "https://thumbs.dreamstime.com/b/muestra-ahora-de-alquiler-74298832.jpg",
+                "HiRing Colombia",
+                "ha compartido",
+                "una actualización: Empresas están buscando...",
+                "24h"
+            )
+        )
+
+    }
+}
---
+++ Deber03/app/src/main/java/com/example/deber03/ContenidoPublicaciones.kt
@@ -0,0 +1,45 @@
+package com.example.deber03
+
+class ContenidoPublicaciones {
+
+    companion object{
+        val publicacionesList = listOf(
+            Publicaciones(
+                "FIS - EPN",
+                "https://media.licdn.com/dms/image/v2/C4E0BAQFIRTvxhYSpqw/company-logo_200_200/company-logo_200_200/0/1630618953804?e=2147483647&v=beta&t=79PLQIl0w_i2o92PENenmpKHm7btIUvlc2r8F9hyACg",
+                "Instititucion de Educacion",
+                "+ Seguir",
+                "En la Facultad de Ingeneiria de Sistemas en la EPN te xplicamos como funciona la nueva acreditacion",
+                "7 horas",
+                "https://media.licdn.com/dms/image/v2/D4E22AQHxxHpjyO8qOA/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1723588403060?e=2147483647&v=beta&t=pa3Nexzn_BQkJEQdExrU9bN-aKXRstoYoDRKVgQwHk4"
+            ),
+            Publicaciones(
+                "Sang Guun Yoo, Ph.D.",
+                "https://media.licdn.com/dms/image/v2/D4E03AQFoU-EWCNBCsQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1676097820888?e=1729728000&v=beta&t=ecw0JBwk-jNFDaejlOux2a8u3NkSYCY9NwO9H0OuPtY",
+                "Profesor en EPN",
+                "+ Seguir",
+                "Deberias saber que:",
+                "3 dias",
+                "https://media.licdn.com/dms/image/v2/D4E22AQEDTid6a06XkA/feedshare-shrink_1280/feedshare-shrink_1280/0/1723213208838?e=1726704000&v=beta&t=_rxSEgZowHN5Gx1HE0gGX1kesQ8za2_8e72sMw2Ripw"
+            ),
+            Publicaciones(
+                "Ola Bini",
+                "https://media.licdn.com/dms/image/v2/C5603AQFGVxKTpfnYcQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1516285362932?e=1729728000&v=beta&t=LmCx9Mknon8BEDGOYCq4kMpNdwylsfUVlMWHuWWf8vg",
+                "OSCP | CEH | CASP+ | ....",
+                "+ Seguir",
+                "Algun vez has visitado sitios web del gobierno? Por ejemplo, lo de la presidencia. O SRI. Quizas MinTel o EcuCert? Has notado que las paginas son un poquito pesados y lentos? ",
+                "4 horas",
+                "https://media.licdn.com/dms/image/v2/D4E22AQEN3Xh-bmEKEw/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1722612886950?e=1726704000&v=beta&t=gb27171VDmN3ZwXw9iAFOJKdud8g0WG1FmHQ80y9NjE"
+            ),
+            Publicaciones(
+                "Bussiness Inphographics",
+                "https://www.euroresidentes.com/empresa/gestion/wp-content/uploads/sites/15/2007/10/empresarios-mediaticos.jpg",
+                "Te comparto cursos de IA y tecnología | Promuevo...",
+                "+ Seguir",
+                "Top cursos gratuitos para aprender de IA y tecnología",
+                "5 días",
+                "https://media.licdn.com/dms/image/D5622AQHuIItXN0YGww/feedshare-shrink_800/0/1723473271320?e=1726704000&v=beta&t=FlZXGLNOezfi24koC89KES4WWKE04zo6usKXTpqBA48"
+            ),
+        )
+    }
+}
